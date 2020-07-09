8b9d32a01fcb6fb33d71402ee04dcc15cae03ce7
Максим <megorov@neoflex.ru>
2020-07-09 17:49:10 +0400

    удалил лишний класс

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AddContactTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AddContactTests.java
@@ -45,54 +45,6 @@ public class AddContactTests {
     wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
   }
 
-  private void fillContactFormFull(ContactDataFull contactDataFull) {
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(contactDataFull.getFirstname());
-    wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys(contactDataFull.getMiddlename());
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(contactDataFull.getLastname());
-    wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys(contactDataFull.getNickname());
-    wd.findElement(By.name("title")).clear();
-    wd.findElement(By.name("title")).sendKeys(contactDataFull.getTitle());
-    wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys(contactDataFull.getCompany());
-    wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(contactDataFull.getAddress());
-    wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys(contactDataFull.getHomePhone());
-    wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys(contactDataFull.getMobile());
-    wd.findElement(By.name("work")).clear();
-    wd.findElement(By.name("work")).sendKeys(contactDataFull.getWorkPhone());
-    wd.findElement(By.name("fax")).clear();
-    wd.findElement(By.name("fax")).sendKeys(contactDataFull.getFax());
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(contactDataFull.getEmail());
-    wd.findElement(By.name("email2")).clear();
-    wd.findElement(By.name("email2")).sendKeys(contactDataFull.getEmail2());
-    wd.findElement(By.name("email3")).clear();
-    wd.findElement(By.name("email3")).sendKeys(contactDataFull.getEmail3());
-    wd.findElement(By.name("homepage")).clear();
-    wd.findElement(By.name("homepage")).sendKeys(contactDataFull.getHomepage());
-    new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactDataFull.getBday());
-    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(contactDataFull.getBmonth());
-    wd.findElement(By.name("byear")).clear();
-    wd.findElement(By.name("byear")).sendKeys(contactDataFull.getByear());
-    new Select(wd.findElement(By.name("aday"))).selectByVisibleText(contactDataFull.getAday());
-    new Select(wd.findElement(By.name("amonth"))).selectByVisibleText(contactDataFull.getAmonth());
-    wd.findElement(By.name("ayear")).clear();
-    wd.findElement(By.name("ayear")).sendKeys(contactDataFull.getAyear());
-    new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactDataFull.getGroup());
-    wd.findElement(By.name("address2")).clear();
-    wd.findElement(By.name("address2")).sendKeys(contactDataFull.getAddress2());
-    wd.findElement(By.name("phone2")).clear();
-    wd.findElement(By.name("phone2")).sendKeys(contactDataFull.getPhone2());
-    wd.findElement(By.name("notes")).clear();
-    wd.findElement(By.name("notes")).sendKeys(contactDataFull.getNotes());
-  }
-
   private void logout() {
     wd.findElement(By.linkText("Logout")).click();
   }
