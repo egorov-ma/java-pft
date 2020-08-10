0d83d9990e5e062cefd31d67cc0f8a76e704d4c6
Максим <megorov@neoflex.ru>
2020-08-10 12:01:31 +0400

    Задание №9: Реализовать проверки путём сравнения списков

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelp.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelp.java
@@ -45,8 +45,8 @@ public class ContactHelp extends BaseHelp {
         }
     }
 
-    public void initContactModification() {
-        click(By.xpath("//img[@alt='Edit']"));
+    public void initContactModification(int index) {
+        wd.findElements(By.xpath("//img[@alt='Edit']")).get(index).click();
     }
 
     public void submitContactModification() {
